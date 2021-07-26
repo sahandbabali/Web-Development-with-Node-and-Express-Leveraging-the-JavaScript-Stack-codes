6601eb6de197967171d1570e15509df40fc60e7c
sahandbabali <sahand.babali@gmail.com>
2021-07-26 21:41:54 +0430

    after adding forms and routes

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,7 @@
 var fortune = require("./lib/fortune");
 var express = require("express");
+var formidable = require("formidable");
+
 var app = express();
 
 // set up handlebars view engine
@@ -17,7 +19,7 @@ app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
-
+app.use(require("body-parser")());
 app.use(function (req, res, next) {
   res.locals.showTests =
     app.get("env") !== "production" && req.query.test === "1";
@@ -38,6 +40,40 @@ app.get("/headers", function (req, res) {
   var s = "";
   for (var name in req.headers) s += name + ": " + req.headers[name] + "\n";
   res.send(s);
+});
+
+app.get("/newsletter", function (req, res) {
+  // we will learn about CSRF later...for now, we just
+  // provide a dummy value
+  res.render("newsletter", { csrf: "CSRF token goes here" });
+});
+
+app.post("/process", function (req, res) {
+  console.log("Form (from querystring): " + req.query.form);
+  console.log("CSRF token (from hidden form field): " + req.body._csrf);
+  console.log("Name (from visible form field): " + req.body.name);
+  console.log("Email (from visible form field): " + req.body.email);
+  res.redirect(303, "/thank-you");
+});
+
+app.get("/contest/vacation-photo", function (req, res) {
+  var now = new Date();
+  res.render("contest/vacation-photo", {
+    year: now.getFullYear(),
+    month: now.getMonth(),
+  });
+});
+
+app.post("/contest/vacation-photo/:year/:month", function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    if (err) return res.redirect(303, "/error");
+    console.log("received fields:");
+    console.log(fields);
+    console.log("received files:");
+    console.log(files);
+    res.redirect(303, "/thank-you");
+  });
 });
 
 // 404 catch-all handler (middleware)
